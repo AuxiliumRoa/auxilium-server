03dd6ffa7f9544fbde491400e8c2d4b38e415bdd
Broc Harcourt <broc.harcourt@gmail.com>
2016-06-16 19:40:43 +1200

    initial express server setup

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import http from 'http'
+import express from 'express'
+import session from 'express-session'
+
+const app = express()
+const server = http.createServer(app)
+const port = 3000
+
+app.use(session({
+	secret: 'Auxilium Roa',
+  saveUninitialized: true,
+  resave: true,
+  store: store
+}))
+
+app.get('/api/actions', (req, res) => {
+	console.log('GET /api/actions')
+	res.json({ actions: 'actions' })
+})
+
+export default function startServer() {
+	server.listen(port, () => {
+		console.log('Auxilium Server Application is listening on port', port)
+	})
+}
