d87125cd37f1f0ac9fc59e003427775675be86b4
Broc Harcourt <broc.harcourt@gmail.com>
2016-06-19 14:41:06 +1200

    add comments table

---
+++ migrations/20160619143423_createCommentsTable.js
@@ -0,0 +1,13 @@
+knexports.up = function(knex, Promise) {
+  return knex.schema.createTableIfNotExists('comments', function (table) {
+  	table.increments('id')
+  	table.integer('action_id')
+  	table.integer('user_id')
+  	table.string('comment')
+  	table.timestamps()
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('comments')
+}
