d3e1461cfab34b9f783bdadfe05587bfe2ae9b9a
Broc Harcourt & surya-surendranath <pair+broc-harcourt.surya-surendranath@devbootcamp.com>
2016-06-19 17:18:08 +1200

    add working /api/joined-actions api endpoint

--- src/api-routes.js
+++ src/api-routes.js
@@ -4,6 +4,14 @@ export default function APIRoutes (model) {
 
 	const router = Router()
 
+	router.get('/user', (req, res) => {
+		console.log('GET /api/user')
+		let user = (req.session.passport && req.session.passport.user)
+			? { name: req.session.passport.user.name }
+			: null
+		res.json({ user: { name: user.name } })
+	})
+
 	router.get('/actions', (req, res) => {
 		console.log('GET /api/actions')
 		model.actions.getAll()
@@ -16,12 +24,20 @@ export default function APIRoutes (model) {
 			})
 	})
 
-	router.get('/user', (req, res) => {
-		console.log('GET /api/user')
-		let user = (req.session.passport && req.session.passport.user)
-			? { name: req.session.passport.user.name }
-			: null
-		res.json({ user: { name: user.name } })
+	router.get('/joined-actions', (req, res) => {
+		console.log('GET /api/joined-actions')
+		if (req.session.passport && req.session.passport.user) {
+			model.actions.getJoined(req.session.passport.user.id)
+				.then((joinedActions) => {
+					res.json(joinedActions)
+				})
+				.catch((error) => {
+					console.log('Error during model.actions.getJoined():', error)
+					res.json({ error: 'Server error.' })
+				})
+		} else {
+			res.json({ error: 'Server error: Not logged in.' })
+		}
 	})
 
 	return router
