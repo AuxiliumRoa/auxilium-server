6a8727e65cd69af0bc1933921a8f9bf72a74c0eb
Broc Harcourt <broc.harcourt@gmail.com>
2016-06-20 13:12:20 +1200

    flesh out reducer post redux planning session

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -2,42 +2,65 @@ import clone from 'clone'
 
 const INITIAL_STATE = { 
   user: null,
-  actions: [],
-  joinedActions: [],
+  actions: {},
+  joinedActions: {},
   displayedAction: 0,
-  fetchedActions: false
+  displayedJoinedAction: 0,
+  fetchedUser: false,
+  fetchedActions: false,
+  fetchedJoinedActions: false,
 }
 
-export default function reducer (state = INITIAL_STATE, action) {
+export default function reducer (state = INITIAL_STATE, reduxAction) {
 
   let newState = clone (state)
 
-  switch (action.type) {
+  switch (reduxAction.type) {
 
-    case 'SET_USER' : 
-      newState.user = action.user
-      if (newState.user && newState.actions.length > 0) {
-        newState.fetchedActions = true
-      }
+    case 'POPULATE_USER' : 
+      newState.user = reduxAction.user
+      newState.fetchedUser = true
       break
 
     case 'POPULATE_ACTIONS' :
-      newState.actions = action.actions
-      if (newState.user && newState.actions.length > 0) {
-        newState.fetchedActions = true
-      }
+      reduxAction.actions.forEach((action) => {
+        newState.actions[action.id] = action
+      })
+      newState.fetchedActions = true
       break
 
     case 'POPULATE_JOINED_ACTIONS' :
-      newState.joinedActions = action.joinedActions
+      reduxAction.joinedActions.forEach((action) => {
+        newState.joinedActions[action.id] = action
+      })
+      newState.fetchedJoinedActions = true
       break
 
     case 'INCREMENT_DISPLAYED_ACTION' :
-      newState.displayedAction = (state.displayedAction + 1) % state.actions.length
+      newState.displayedAction = (state.displayedAction + 1) % Object.keys(state.actions).length
       break
 
-    case 'ADD_JOINED_ACTION' :
-      newState.joinedActions = state.joinedActions.concat(action.action)
+    case 'JOIN_ACTION' :
+      newState.joinedActions[reduxAction.action.id] = state.actions[reduxAction.action.id]
+      newState.joinedActions[reduxAction.action.id].comments = reduxAction.action.comments
+      newState.joinedActions[reduxAction.action.id].currentComment = ''
+      delete newState.actions[reduxAction.action.id]
+      newState.displayedAction = state.displayedAction % Object.keys(newState.actions).length
+      break
+
+    case 'SET_DISPLAYED_JOINED_ACTION' :
+      break
+
+    case 'UNJOIN_ACTION' :
+      break
+
+    case 'ADD_COMMENT' :
+      break
+
+    case 'SET_CURRENT_COMMENT' :
+      break
+
+    case 'ADD_ACTION' :
       break
 
   }
