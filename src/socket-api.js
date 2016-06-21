a4e6dbeae677ab502222774951c080fc76c5e2a7
Broc Harcourt <broc.harcourt@gmail.com>
2016-06-21 17:44:22 +1200

    rough out replacing json api with socket usage

--- src/socket-api.js
+++ src/socket-api.js
@@ -7,6 +7,75 @@ export default function configureSocketAPI (server, model) {
 	io.on('connection', (socket) => {
 		console.log('SOCKET connection')
 
+		socket.on('request-user', (data) => {
+			console.log('SOCKET request-user')
+			if (loggedIn(data)) {
+				res.json({ user: req.session.passport.user })
+			}
+		})
+
+		socket.on('request-actions', (data) => {
+		console.log('SOCKET request-actions')
+		if (loggedIn(data)) {
+			model.actions.getNotJoined(req.session.passport.user.id)
+				.then((actions) => {
+					res.json(actions)
+				})
+				.catch((error) => {
+					handleError(data, error)
+				})
+			}
+		})
+
+		socket.on('request-joined-actions', (data) => {
+		console.log('SOCKET request-joined-actions')
+		if (loggedIn(data)) {
+			model.actions.getJoined(req.session.passport.user.id)
+				.then((joinedActions) => {
+					res.json(joinedActions)
+				})
+				.catch((error) => {
+					handleError(data, error)
+				})
+			}
+		})
+
+		// socket.on('join-action', (data) => {
+		// console.log('SOCKET /api/join-action')
+		// if (loggedIn(data)) {
+		// 	model.actions.joinAction(req.session.passport.user.id, req.query.id)
+		// 		.then((joinedAction) => {
+		// 			console.log('joinedAction', joinedAction)
+		// 			res.json(joinedAction)
+		// 		})
+		// 		.catch((error) => {
+		// 			handleError(data, error)
+		// 		})
+		// 	}
+		// })
+
+		// socket.on('unjoin-action', (data) => {
+		// console.log('SOCKET /api/unjoin-action')
+		// if (loggedIn(data)) {
+		// 	model.actions.unjoinAction(req.session.passport.user.id, req.query.id)
+		// 		.then(() => {
+		// 			res.json({ status: 'OK' })
+		// 		})
+		// 		.catch((error) => {
+		// 			handleError(data, error)
+		// 		})
+		// 	}
+		// })
+
+	// function loggedIn (req, res) {
+	// 	if (req.session.passport && req.session.passport.user) {
+	// 		return true
+	// 	} else {
+	// 		res.json({ error: 'You must be logged in to access this endpoint.' })
+	// 		return false
+	// 	}
+	// }
+
 		socket.on('action', (reduxAction) => {
 			console.log('SOCKET action', reduxAction)
 
