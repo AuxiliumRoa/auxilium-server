5f3179487d57eb2141d285cdb6c8e7e824a3c6eb
Broc Harcourt <broc.harcourt@gmail.com>
2016-06-22 12:41:55 +1200

    logout endpoint in passport.js

--- src/passport.js
+++ src/passport.js
@@ -76,4 +76,11 @@ export default function configurePassport (app, model) {
     
   })
 
+  app.get('/auth/logout', (req, res, next) => {
+    req.session.destroy((err) => {
+      console.log('Server error:', err)
+    })
+    res.redirect('/')
+  })
+
 }
